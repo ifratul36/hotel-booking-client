8da695b3d61f6e4f6484c1aeaf2dd577e77ce6c6
ifratul36 <jannatrituwd@gmail.com>
2024-12-26 19:15:02 +0600

    Review System: Implement review posting feature with rating and comment

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -35,24 +35,7 @@ const ErrorPage = () => {
     //               strokeLinejoin='round'
     //               d='M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18'
     //             />
-    //           </svg>
-
-    //           <span>Go back</span>
-    //         </button>
-
-    //         <Link
-    //           to='/'
-    //           className='w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-gray-500 rounded-lg shrink-0 sm:w-auto hover:bg-gray-600'
-    //         >
-    //           Take me home
-    //         </Link>
-    //       </div>
-    //     </div>
-
-    //     <div className='relative w-full mt-8 lg:w-1/2 lg:mt-0'>
-    //       <img
-    //         className=' w-full lg:h-[32rem] h-80 md:h-96 rounded-lg object-cover '
- 
+  
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
     {/* Background GIF/Image */}
     <div className="relative">
